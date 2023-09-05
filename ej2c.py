44d1ce74ee83277da3a61e321d2da917471b840a
Bruno Castagnino Rossi <87082300+BCastagninoRossi@users.noreply.github.com>
2023-09-05 16:03:22 -0300

    ejercicio 2c

--- ej2c.py
+++ ej2c.py
@@ -1,5 +1,6 @@
 
 import numpy as np
+import scipy.stats as stats
 from scipy.stats import kstest, shapiro
 
 
@@ -24,16 +25,36 @@ class1_wid = np.array(c1_width)
 class2_wid = np.array(c2_width)
 
 
-# def cuantizacion( data, steps = 20):
-#     """
-#     Cuantización del vector <data> en <steps> niveles.
-#     """
-#     if steps < 2:
-#         steps = 2
-#     bins = np.arange(0.0,1.0+(1/steps), 1/steps)
-#     bins = (np.max(data)-np.min(data))*bins + np.min(data) #determina los límites de cada bin-
-#     bins = bins[1:-1]
-#     return np.digitize(data,bins)
+def cuantizacion( data, steps = 20):
+    """
+    Cuantización del vector <data> en <steps> niveles.
+    """
+    if steps < 2:
+        steps = 2
+    bins = np.arange(0.0,1.0+(1/steps), 1/steps)
+    bins = (np.max(data)-np.min(data))*bins + np.min(data) #determina los límites de cada bin-
+    bins = bins[1:-1]
+    return np.digitize(data,bins)
 
-# print(class1_len)
-# print(cuantizacion(class1_len))
+print(class1_len)
+print(cuantizacion(class1_len))
+
+cuant_1_len = cuantizacion(class1_len, steps=10)
+cuant_1_wid = cuantizacion(class1_wid, steps=10)
+
+# print(f'x: \n {cuan}')
+# print(f'norm_x: \n {cuant_1_len}')
+# print(f'y: \n {y}')
+# print(f'norm_y: \n {cuant_1_wid}')
+
+table = stats.contingency.crosstab(cuant_1_len, cuant_1_wid)
+st = stats.chi2_contingency(table[1])
+print(f'p:{st[1]}')
+print(f'rango_x: {table[0][0]}')
+print(f'rango_y: {table[0][1]}')
+print(f'frecuencias:\n {table[1]}')
+if st[1] > 0.05:
+    print(f'Se acepta H0(independencia). st: {st[0]}, p: {st[1]}')
+else:
+    print(f'Se rechaza H0, st: {st[0]}, p: {st[1]}')
+print('\n')
